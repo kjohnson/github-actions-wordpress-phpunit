6a317dbe8e0f1f99dc63af978423db5495b3b31b
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-12-07 12:53:50 -0500

    chore: Scaffold plugin with WP-CLI.

---
+++ github-actions-wordpress-phpunit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Github Actions Wordpress Phpunit
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     github-actions-wordpress-phpunit
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Github_Actions_Wordpress_Phpunit
+ */
+
+// Your code starts here.
