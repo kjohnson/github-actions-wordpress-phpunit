c8631f425b0e5058367d5f92e07de25fb37509a9
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-12-07 14:50:39 -0500

    wip: Add test.

---
+++ tests/test-plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Github_Actions_Wordpress_Phpunit
+ */
+
+/**
+ * Sample test case.
+ */
+class PluginTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_true() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
